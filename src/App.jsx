f6449997cc4e070d1f8f537adf63c57064b74155
Akshay Negi <akshaynegi18@gmail.com>
2025-06-08 01:16:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth';
+import {login,logout} from './store/authSlice';
+import { Footer,Header } from './components';
+import './App.css'
+
+function App() {
+ const [loading,setLoading] = useState(true);
+ const dispatch = useDispatch();
+
+ useEffect(()=>{
+   authService.getCurrentUser().then((response)=>{
+     if(response){
+       dispatch(login({userData:response}));
+     }else{
+       dispatch(logout());
+     }
+    
+   })
+   .finally(
+    ()=>{setLoading(false)}
+  )
+ },[])
+
+  return !loading?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+         {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ):null
+}
+
+export default App
